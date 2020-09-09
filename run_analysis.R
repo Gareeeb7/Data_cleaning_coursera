39e7f27adf95f452cfffbab495ba46d22907da49
Gareeeb7 <kunal9988243618@gmail.com>
2020-09-09 21:43:41 +0530

    text Data Preprocesed and Labeled

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+#Name ~ Kunal Malhotra 
+#Date ~ 09-09-2020
+#Process ~ Preprocessing and Data Cleaning
+
+#Set to be used : https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+#checking the working directories
+
+list.files("C:\\Users\\user\\Desktop\\R_files\\UCI HAR Dataset\\")
